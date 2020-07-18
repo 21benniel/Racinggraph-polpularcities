1ce78437da8afb43486d20989aad3c615f224c80
21benniel <johnbenniel7@gmail.com>
2020-07-18 11:39:54 +0530

    first commit

---
+++ barracechart.py
@@ -0,0 +1,48 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import matplotlib.animation as animation
+from IPython.display import HTML
+
+url = "Z:\Machine learning\city_populations_projections.csv"
+df = pd.read_csv(url, usecols=['name', 'group', 'year', 'value'])
+print(df.tail())
+
+colors = dict(zip(
+    ["India", "Europe", "Asia", "Latin America", "Middle East", "North America", "Africa"],
+    ["#adb0ff", "#ffb3ff", "#90d595", "#e48381", "#aafbff", "#f7bb5f", "#eafb50"]
+))
+group_lk = df.set_index('name')['group'].to_dict()
+
+fig, ax = plt.subplots(figsize=(15, 8))
+
+def draw_barchart(current_year):
+    dff = df[df['year'].eq(current_year)].sort_values(by='value', ascending=True).tail(10)
+    ax.clear()
+    ax.barh(dff['name'], dff['value'], color=[colors[group_lk[x]] for x in dff['name']])
+    dx = dff['value'].max() / 200
+    for i, (value, name) in enumerate(zip(dff['value'], dff['name'])):
+        ax.text(value-dx, i,     name,           size=14, weight=600, ha='right', va='bottom')
+        ax.text(value-dx, i-.25, group_lk[name], size=10, color='#444444', ha='right', va='baseline')
+        ax.text(value+dx, i,     f'{value:,.0f}',  size=14, ha='left',  va='center')
+    ax.text(1, 0.4, current_year, transform=ax.transAxes, color='#777777', size=46, ha='right', weight=800)
+    ax.text(0, 1.06, 'Population (thousands)', transform=ax.transAxes, size=12, color='#777777')
+    ax.xaxis.set_major_formatter(ticker.StrMethodFormatter('{x:,.0f}'))
+    ax.xaxis.set_ticks_position('top')
+    ax.tick_params(axis='x', colors='#777777', labelsize=12)
+    ax.set_yticks([])
+    ax.margins(0, 0.01)
+    ax.grid(which='major', axis='x', linestyle='-')
+    ax.set_axisbelow(True)
+    ax.text(0, 1.15, 'Most populous cities in the world from 1575 to 2015',
+            transform=ax.transAxes, size=24, weight=600, ha='left', va='top')
+    ax.text(1, 0, '', transform=ax.transAxes, color='#777777', ha='right',
+            bbox=dict(facecolor='white', alpha=0.8, edgecolor='white'))
+    plt.box(False)
+    
+draw_barchart(2018)
+fig, ax = plt.subplots(figsize=(15, 8))
+animator = animation.FuncAnimation(fig, draw_barchart, frames=range(1575, 2015))
+plt.show()
+animator.save("Mostpopulatedcities.mp4")
